e6c813c8bd6ab3e6b2fb9dfca48fc2db2d98d807
Hemuryel <hyl.silva@gmail.com>
2022-11-03 11:20:06 -0300

    Implementação Template Method

---
+++ DDGofTemplateMethod/ITemplateDeImpostoCondicional.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDGofTemplateMethod
+{
+    public abstract class ITemplateDeImpostoCondicional : IImposto
+    {
+        public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
+
+        public double Calcular(Orcamento orcamento)
+        {
+            if (DeveUsarMaximaTaxacao(orcamento))
+            {
+                return MaximaTaxacao(orcamento);
+            }
+
+            return MinimaTaxacao(orcamento);
+        }
+    }
+}
--- DDGofTemplateMethod/Program.cs
+++ DDGofTemplateMethod/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            IImposto iccc = new ICCC();
+
+            Orcamento orcamento = new Orcamento(3001.0);
+
+            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+            calculador.RealizarCalculo(orcamento, iccc);
+
+            Console.ReadKey();
         }
     }
 }
