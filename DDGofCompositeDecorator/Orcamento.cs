06b0bc3206a3811f8a3f34d809c04599888c2860
Hemuryel <hyl.silva@gmail.com>
2022-11-07 17:28:18 -0300

    Alterações

--- DDGofCompositeDecorator/Orcamento.cs
+++ DDGofCompositeDecorator/Orcamento.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DDGofComposite
+namespace DDGofCompositeDecorator
 {
     public class Orcamento
     {
--- DDGofCompositeDecorator/Program.cs
+++ DDGofCompositeDecorator/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace DDGofComposite
+namespace DDGofCompositeDecorator
 {
     public class Program
     {
