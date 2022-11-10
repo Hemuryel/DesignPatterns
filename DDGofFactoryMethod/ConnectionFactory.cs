c68b95fb9c05c3e3e426464ba1e1998132dd6d7f
Hemuryel <hyl.silva@gmail.com>
2022-11-10 15:53:46 -0300

    Atualização.

--- DDGofFactoryMethod/ConnectionFactory.cs
+++ DDGofFactoryMethod/ConnectionFactory.cs
@@ -14,6 +14,8 @@ namespace DDGofFactoryMethod
          * 
          * isola a criação de objetos complicados
          * 
+         * faça uso de Factories sempre que a criação de um objeto possa mudar em algum momento
+         * 
          * obs: enquanto Builder, o objeto que é criado varia de acordo com os parâmetros
          */
 
