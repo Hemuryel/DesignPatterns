94ecc3df2953f126dc8c90351d28775536bdeaa2
Hemuryel <hyl.silva@gmail.com>
2022-11-11 11:26:46 -0300

    Alterações.

---
+++ DDGofAdapter/GeradorXMLAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DDGofAdapter
+{
+    public class GeradorXMLAdapter
+    {
+        public string GerarXml(object o)
+        {
+            XmlSerializer serializer = new XmlSerializer(o.GetType());
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, o);
+            string xml = writer.ToString();
+
+            return xml;
+        }
+    }
+}
--- DDGofAdapter/Program.cs
+++ DDGofAdapter/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace DDGofAdapter
+﻿using System.Xml.Serialization;
+
+namespace DDGofAdapter
 {
     internal class Program
     {
+        /* Adapter
+         * 
+         * lidar com código legado
+         * converter interface legada em outra
+         */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Fulano";
+            cliente.Endereco = "Rua Vergueiro";
+            cliente.DtNascimento = DateTime.Now;
+
+            string xml = new GeradorXMLAdapter().GerarXml(cliente);
+
+            Console.WriteLine(xml);
         }
     }
 }
