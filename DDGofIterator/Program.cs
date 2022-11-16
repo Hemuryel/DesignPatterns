8dfbcf1bb6816eabc0ff0c9da4c244a29d69d68e
Hemuryel <hyl.silva@gmail.com>
2022-11-16 08:34:10 -0300

    Alterações

---
+++ DDGofIterator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDGofIterator
+{
+    /* Iterator
+     * 
+     * permite o acesso sequencial aos elementos de um conjunto 
+     * sem expor sua implementação subjacente
+     */
+
+    public class Program
+    {
+
+    }
+}
